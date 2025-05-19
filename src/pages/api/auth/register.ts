fa2c2cdeb91192771b3f0dce3c1305ad202ad610
Serge Basangovs <sergejs.basangovs@artfinder.com>
2025-05-19 08:24:05 +0100

    fix: replaced bcrypt by bcryptjs package

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 import type { NextApiRequest, NextApiResponse } from "next"
 import type { NextApiErrorResponse, NextApiSuccessResponse } from "@/types"
